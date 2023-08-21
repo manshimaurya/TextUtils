aac72bcf850434543b26fcaeb21728ff7d296eb0
manshimaurya <126577007+manshimaurya@users.noreply.github.com>
2023-08-21 09:54:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React,{ useState } from 'react';
 import './App.css';
-//import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-//import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light');//darkmode is enable or not
+  const [mode, setMode] = useState('light');//darkmode is enable or n
   const [alert, setAlert] = useState(null);
 
   const showAlert=(message,type)=>{
@@ -28,13 +26,6 @@ function App() {
       document.body.style.backgroundColor='#303d76'//dark
       showAlert("  Dark mode is enabled !","success")
       document.title="Textutils - Dark Mode";
-      /*  setInterval(()=>{
-          document.title="textutils is amazing mode";
-        },2000);
-        setInterval(()=>{
-          document.title="textutils is amazing mode";
-        },1000);*/
-    }
     else{
       setMode('light')
       document.body.style.backgroundColor='#ebbebe'//light
@@ -44,26 +35,13 @@ function App() {
   };
   return (
     <>
-   {/*<BrowserRouter>*/}
     <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert}/>
     <div className="container my-3" mode={mode}>
     <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-      {/*<Routes>
-          <Route exact path="/about" element={<About/>}/>
-      </Routes>
-      <Routes>
-            <Route exact
-              path="/"
-              element={
-                */}
-                
-              {/*}</div>
-            />
-            </Routes>*/}
-          
+     
       </div>
-   {/*</BrowserRouter>  */}
+ 
     </>
   );
 }
